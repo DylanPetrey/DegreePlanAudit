a8a71f7728c71af2e084ddd0ceb8ce91ec2a0bca
SunniM <sunnismagan@gmail.com>
2022-10-16 21:09:10 -0500

    added accessor methods

--- src/Form.java
+++ src/Form.java
@@ -4,7 +4,7 @@ enum Concentration {
     TRADITIONAL,
     NETWORKS,
     INTEL,
-    CYBER,
+    CYBERSEC,
     INTERACTIVE,
     SYSTEMS,
     DATA_SCIENCE,
@@ -17,25 +17,38 @@ public class Form {
     private String studentID;
     private String semesterAdmitted;
     private String anticipatedGraduation;
-    private boolean fasttrack;
+    private boolean isFastTrack;
     private boolean thesis;
-    private List<Course> core;
-    private List<Course> addlCore;
+    private List<Course> coreCourses;
+    private List<Course> electiveCourses;
+    private List<Course> addlCourses;
     
 
     /** 
      * Initializes Form object to be used to fill out PDF
-     * @param currentStudent
+     * @param currentStudent Student object that the form is generated for
      */
     public Form(Student currentStudent) {
         this.studentName = currentStudent.getStudentName();
         this.studentID = currentStudent.getStudentId();
         this.semesterAdmitted = "";
         this.anticipatedGraduation = "";
-        this.fasttrack = false;
+        this.isFastTrack = false;
         this.thesis = false;
-
-
-
     }
+
+    /**
+     * Accessor methods to be used outside the class.
+     */
+    public Concentration getConcentration(){ return this.concentration; }
+    public String getStudenName(){return studentName; }
+    public String getStudentID(){ return studentID; }
+    public String getSemesterAdmitted(){ return semesterAdmitted; }
+    public String getAnticipatedGraduation(){ return anticipatedGraduation; }
+    public boolean isFastTrack(){ return isFastTrack; }
+    public boolean isThesis() { return thesis; }
+    public List<Course> getCoreCourses() { return coreCourses; }
+    public List<Course> getElectiveCourses() { return electiveCourses; }
+    public List<Course> getAdditionalCourses() { return addlCourses; }
+    
 }
