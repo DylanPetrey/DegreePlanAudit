e4ee00ddb9ba4cf82bf68f34c7abee5dc19007ac
DylanPetrey <65378181+DylanPetrey@users.noreply.github.com>
2022-12-02 18:40:28 -0600

    fix modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,15 @@
+/*
+    make sure the following command is added to the VM classpath:
+    --add-exports javafx.base/com.sun.javafx.event=org.controlsfx.controls
+*/
+
 module utd.dallas {
-    requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.controls;
+    requires org.controlsfx.controls;
     requires org.apache.pdfbox;
     requires json.simple;
     requires json.path;
-    requires org.controlsfx.controls;
 
     opens utd.dallas.frontend to javafx.fxml;
     exports utd.dallas.frontend;
